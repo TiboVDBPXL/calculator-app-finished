85a19df34e7abdf6ab0496e474bd33899220ca14
TiboVDBPXL <92723605+TiboVDBPXL@users.noreply.github.com>
2022-12-11 12:10:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express();
 
 const mydotenv = require('dotenv').config();
-console.log(mydotenv);
+console.log("env" + mydotenv);
 console.log("env logging:" + process.env.PORT);
 
 app.use(express.static(path.join(__dirname, 'public')))
