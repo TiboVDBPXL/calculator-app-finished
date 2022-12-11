d81a00a3d59bcedee32c7033ead3159f2831d6f2
TiboVDBPXL <92723605+TiboVDBPXL@users.noreply.github.com>
2022-12-11 12:19:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const path = require('path')
 
 const app = express();
 
-const mydotenv = require('dotenv').config({path: '/opt/.env'});
-console.log("env" + mydotenv);
-console.log("env logging:" + process.env.PORT);
+require('dotenv').config({path: '/opt/.env'});
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/calc', routes);
