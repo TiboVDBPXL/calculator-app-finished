5e0c2208332dd0b9f64ea5d2754d8a66565df339
TiboVDBPXL <92723605+TiboVDBPXL@users.noreply.github.com>
2022-12-11 11:58:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
-console.log("env logging:" + process.env.PORT)
 const app = require("./app");
+
+require('dotenv').config();
+console.log("env logging:" + process.env.PORT);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port);
